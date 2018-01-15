3d7ecdffbea357cd030791cd3f682b4097f0e599
Omar Madrid Ontiveros <madro24@Omars-MacBook-Pro.local>
2018-01-14 22:07:47 -0800

    fix environment prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,19 +3,20 @@ export const environment = {
 
   region: 'us-east-1',
 
-        userPoolId: 'us-east-1_oX7QuKhqM',
-        clientId: '696j7pu1ulhtqgu7nf91ea262c',
+  userPoolId: 'us-east-1_oX7QuKhqM',
+  clientId: '696j7pu1ulhtqgu7nf91ea262c',
+  identityPoolId: '',
 
-              rekognitionBucket: '',
-              albumName: "usercontent",
-              bucketRegion: 'us-east-1',
+  rekognitionBucket: '',
+  albumName: "usercontent",
+  bucketRegion: 'us-east-1',
 
-              ddbTableName: 'LoginTrail',
+  ddbTableName: 'LoginTrail',
 
-              cognito_idp_endpoint: '',
-              cognito_identity_endpoint: '',
-              sts_endpoint: '',
-              dynamodb_endpoint: '',
-              s3_endpoint: ''
+  cognito_idp_endpoint: '',
+  cognito_identity_endpoint: '',
+  sts_endpoint: '',
+  dynamodb_endpoint: '',
+  s3_endpoint: ''
 
 };
