548492208ddb52f4417ac5cb0a33befae6005ba5
Omar Madrid <omar.madrid@thermofisher.com>
2018-02-16 17:09:24 -0800

    form component changes

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -12,44 +12,18 @@ const now = new Date();
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent implements OnInit {
-  public mapArray: Array<ServiceItem>;
-  public serviceItem: ServiceItem;
-  public destinationArray: Array<Destination>;
+
+
 
 
   constructor(public router: Router, public serviceItemDDB: ServiceItemDDBService) {
-    this.serviceItem = new ServiceItem();
-    this.serviceItem.date = String(now.getFullYear()) + String(now.getMonth()+1) + String(now.getDate()) ;
-    this.serviceItem.destinations.push(new Destination());
-    this.mapArray = [];
-      this.serviceItemDDB.getServiceAllItems(this.mapArray);
+
+
   }
 
   ngOnInit() {
   }
 
-  addDeliveryService() {
-    console.log("This is my service:", this.serviceItem);
-
-    var itemId = this.serviceItem.date + this.serviceItem.timePicker.hour + this.serviceItem.timePicker.minute + "E1"
-
-    var recolectDate = this.serviceItem.datePicker.year + "-" +this.serviceItem.datePicker.month + "-" + this.serviceItem.datePicker.day;
-
-    var recolectTime = this.serviceItem.timePicker.hour+ ":"+this.serviceItem.timePicker.minute;
-
-
-
-    this.serviceItem.itemId = itemId;
-    this.serviceItem.usedFares.distanceFare = "9";
-    this.serviceItem.usedFares.timeFare = "2.25";
-    this.serviceItem.recolectTime=recolectTime;
-    this.serviceItem.recolectDate=recolectDate;
-    this.serviceItem.destinations[0].sequence = "1";
-    this.serviceItem.itemStatus = "CREATED";
-    this.serviceItem.totalCost = "50";
-    this.serviceItemDDB.writeServiceItem(this.serviceItem);
-    this.mapArray.push(this.serviceItem);
-  }
 
 
 }
